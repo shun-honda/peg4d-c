80ce2fc78a83f7ef67c3a705e99f97870765f2b1
shun-honda <hondashun@honda-no-MacBook-Pro.local>
2014-09-20 23:05:13 +0900

    add StringBuilder

---
+++ peg4d-c/StringBuilder.c
@@ -0,0 +1,40 @@
+//
+//  StringBuilder.c
+//  peg4d-c
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "StringBuilder.h"
+
+void SB_create(StringBuilder *sb)
+{
+	sb->len = 0;
+	sb->extent = 16;
+	sb->str = (char *)malloc(sb->extent);
+	sb->str[0] = '\0';
+}
+
+void SB_delete(StringBuilder *sb)
+{
+	free(sb->str);
+}
+
+void SB_append(StringBuilder *sb, const char *str)
+{
+	int newLen;
+	int newExtent;
+    
+	newLen = sb->len + strlen(str);
+	newExtent = sb->extent;
+	while (newExtent <= newLen) {
+		newExtent <<= 1;
+	}
+	if (sb->extent < newExtent) {
+		sb->extent = newExtent;
+		sb->str = (char *)realloc(sb->str, sb->extent);
+		//fprintf(stderr, "newExtent=[%d]\n", newExtent);
+	}
+	strcpy(sb->str + sb->len, str);
+	sb->len = newLen;
+}
---
+++ peg4d-c/StringBuilder.h
@@ -0,0 +1,15 @@
+//
+//  StringBuilder.h
+//  peg4d-c
+//
+
+#ifndef peg4d_c_StringBuilder_h
+#define peg4d_c_StringBuilder_h
+
+typedef struct {
+	char *str;
+	int len;
+	int extent;
+} StringBuilder;
+
+#endif
