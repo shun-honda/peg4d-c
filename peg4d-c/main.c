fad9a3bbbc9338001ee28a26f8942b9e54fa6a0c
shun-honda <hondashun@honda-no-MacBook-Pro.local>
2014-09-20 13:52:07 +0900

    Initial Commit

---
+++ peg4d-c/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  peg4d-c
+//
+//  Created by 本多 峻 on 2014/09/20.
+//
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
